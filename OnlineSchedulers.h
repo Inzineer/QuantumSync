c7861028a63dbe8c6a1e0a9349c861cf4e23186f
Md Inzamam <102192370+Inzineer@users.noreply.github.com>
2024-09-16 22:54:03 +0530

    Update OnlineSchedulers.h
    Correction in MLFQ!

--- OnlineSchedulers.h
+++ OnlineSchedulers.h
@@ -10,17 +10,20 @@
 #include <stdbool.h>
 #include <string.h>
 #include <fcntl.h>
+#include <stdint.h>
 
 
 typedef struct {
     char *command;
     bool finished;
-    bool error;    
+    bool error; 
+    uint64_t arrival_time;   
     uint64_t start_time;
     uint64_t completion_time;
     uint64_t turnaround_time;
     uint64_t waiting_time;
     uint64_t response_time;
+    uint64_t burst_time;
     bool started; 
     int process_id;
 
@@ -33,38 +36,7 @@ void ShortestJobFirst();
 void ShortestRemainingTimeFirst();
 void MultiLevelFeedbackQueue(int quantum0, int quantum1, int quantum2, int boostTime);
 
-int count=0;
-void process_command(char *command){
-    count++;
-    
-}
-void input() {
-    fd_set read_fds;
-    struct timeval timeout;
-    char buffer[BUFFER_SIZE];
-    FD_ZERO(&read_fds);
-    FD_SET(STDIN_FILENO, &read_fds); 
-    timeout.tv_sec=0;
-    timeout.tv_usec=0;
-    int ready=select(STDIN_FILENO + 1, &read_fds, NULL, NULL, &timeout);
 
-    if (ready==-1){
-        exit(-1);
-    } 
-    else if(ready==0){
-        ;
-    }
-    else{
-        if(FD_ISSET(STDIN_FILENO,&read_fds)){
-            fgets(buffer, sizeof(buffer), stdin);
-            char *command = strtok(buffer, "\n");
-            while (command != NULL) {
-                process_command(command);
-                command = strtok(NULL, "\n");
-            }
-        }
-    }
-}
  
 
 
@@ -175,22 +147,69 @@ void UpgradeAllNode(Queue *Q1,Queue *Q2,Queue *Q3){
     Q3->LastNode=NULL;
 }
 
-void ShortestJobFirst(){
+typedef struct storage{
+    char *cmd;
+    int tr;
+    uint64_t avgbt;
+} storage;
 
+storage map[50];
+int count=0;
+
+void taking_input(char *cmds){
+    fd_set read_fds;
+    struct timeval timeout;
+    FD_ZERO(&read_fds);
+    FD_SET(STDIN_FILENO, &read_fds); 
+    timeout.tv_sec=0;
+    timeout.tv_usec=0;
+    int ready=select(STDIN_FILENO + 1, &read_fds, NULL, NULL, &timeout);
+
+    if (ready==-1){
+        exit(-1);
+    } 
+    else if(ready==0){
+        ;
+    }
+    else{
+        if(FD_ISSET(STDIN_FILENO,&read_fds)){
+            char temp[1024];
+            while (fgets(temp, 1024, stdin)) {
+                strcat(cmds, temp);  
+                if (strcmp(temp, "\n") == 0) {
+                    break;
+                }
+            }
+        }
+    }
 }
 
-
-
 void MultiLevelFeedbackQueue(int quantum0,int quantum1,int quantum2,int boostTime){
+    int fp=0; //count of finished processes
     Process p[50];
-    struct Queue* Q1=createQueue();
-    struct Queue* Q2=createQueue();
-    struct Queue* Q3=createQueue();
-    input();
+    Queue* Q1=createQueue();
+    Queue* Q2=createQueue();
+    Queue* Q3=createQueue();
+    char cmds[1024];
+    memset(cmds, 0, sizeof(cmds));
+    taking_input(cmds);
+    char *token = strtok(cmds,"\n");
+    while ( token!=NULL) {  
+        p[count].command=strdup(token);
+        Node *temp=newNode(count);
+        insertQueue(Q2,temp);
+        p[count].finished=false;
+        p[count].started=false;
+        p[count].error=false;
+        p[count].burst_time=0;
+        p[count].arrival_time=0;
+        count++;
+        token = strtok(NULL, "\n");
+    }
+
     uint64_t start=timeinms();
     char *filename="result_online_MLFQ.csv";
     CreateFile(filename);
-    int count=0;
     uint64_t conttime;
     uint64_t past=0;
     uint64_t present=0;
@@ -209,12 +228,12 @@ void MultiLevelFeedbackQueue(int quantum0,int quantum1,int quantum2,int boostTim
                 else if(r==0){
                     char *input=strdup(p[i].command);
                     char *myargs[512 + 1];
-                    char *token = strtok(input, " ");
+                    char *token1 = strtok(input, " ");
                     int j=0;
-                    while (token != NULL) {  
-                        myargs[j] = token;
+                    while (token1 != NULL) {  
+                        myargs[j] = token1;
                         j++;
-                        token = strtok(NULL, " ");
+                        token1 = strtok(NULL, " ");
                     }
                     myargs[j] = NULL;
                     execvp(myargs[0],myargs);
@@ -229,20 +248,28 @@ void MultiLevelFeedbackQueue(int quantum0,int quantum1,int quantum2,int boostTim
                 } 
             }
             uint64_t aft;
-            usleep(quantum0*1000);
             int status;
-            int res=waitpid(p[i].process_id,&status,WNOHANG);
+            int res;
+            if(fp==count-1){
+                res=waitpid(p[i].process_id,&status,0);
+            }
+            else{
+                usleep(quantum0*1000);
+                res=waitpid(p[i].process_id,&status,WNOHANG);
+            }
+
             if(res==0){
                 kill(p[i].process_id,SIGSTOP);
                 aft=timeinms()-start;
                 p[i].burst_time+=aft-conttime;
-                struct Node* temp=newNode(i);
+                Node* temp=newNode(i);
                 insertQueue(Q2,temp);
             } 
             else if(!WIFEXITED(status) || WEXITSTATUS(status)!=0){
                 p[i].error=true;
-                p[i].finished=true;
+                p[i].finished=false;
                 aft=timeinms()-start;
+                insert(p[i],filename);
             }
             else{
                 aft=timeinms()-start;
@@ -253,7 +280,21 @@ void MultiLevelFeedbackQueue(int quantum0,int quantum1,int quantum2,int boostTim
                 p[i].burst_time+=aft-conttime;
                 p[i].waiting_time=p[i].turnaround_time-p[i].burst_time;
                 insert(p[i],filename);
-                count++;
+                bool flag=false;
+                for(int j=0;j<fp;j++){
+                    if(strcmp(map[j].cmd,p[i].command)==0){
+                        map[j].avgbt=(map[j].tr*map[j].avgbt+ p[i].burst_time)/(map[j].tr+1);
+                        map[j].tr++;
+                        flag=true;
+                        break;
+                    }
+                }
+                if(!flag){
+                    map[fp].avgbt=p[i].burst_time;
+                    map[fp].cmd=strdup(p[i].command);
+                    map[fp].tr=1;
+                }
+                fp++;
             }
             RemoveNode(Q1);
             printf("In Priority 1 Queue | Command: %s | start_time of context: %ld | completion_time of context: %ld\n",p[i].command,conttime,aft);
@@ -261,6 +302,7 @@ void MultiLevelFeedbackQueue(int quantum0,int quantum1,int quantum2,int boostTim
         }
         else if(Q2->FirstNode!=NULL){
             int i=Q2->FirstNode->index;
+
             if(p[i].started){
                 conttime=timeinms()-start;
                 kill(p[i].process_id,SIGCONT);
@@ -273,12 +315,12 @@ void MultiLevelFeedbackQueue(int quantum0,int quantum1,int quantum2,int boostTim
                 else if(r==0){
                     char *input=strdup(p[i].command);
                     char *myargs[512 + 1];
-                    char *token = strtok(input, " ");
+                    char *token2 = strtok(input, " ");
                     int j=0;
-                    while (token != NULL) {  
-                        myargs[j] = token;
+                    while (token2 != NULL) {  
+                        myargs[j] = token2;
                         j++;
-                        token = strtok(NULL, " ");
+                        token2 = strtok(NULL, " ");
                     }
                     myargs[j] = NULL;
                     execvp(myargs[0],myargs);
@@ -293,20 +335,28 @@ void MultiLevelFeedbackQueue(int quantum0,int quantum1,int quantum2,int boostTim
                 } 
             }
             uint64_t aft;
-            usleep(quantum1*1000);
             int status;
-            int res=waitpid(p[i].process_id,&status,WNOHANG);
+            int res;
+            if(fp==count-1){
+                res=waitpid(p[i].process_id,&status,0);
+            }
+            else{
+                usleep(quantum1*1000);
+                res=waitpid(p[i].process_id,&status,WNOHANG);
+            }
+
             if(res==0){
                 kill(p[i].process_id,SIGSTOP);
                 aft=timeinms()-start;
                 p[i].burst_time+=aft-conttime;
-                struct Node* temp=newNode(i);
+                Node* temp=newNode(i);
                 insertQueue(Q3,temp);
             } 
             else if(!WIFEXITED(status) || WEXITSTATUS(status)!=0){
                 p[i].error=true;
-                p[i].finished=true;
+                p[i].finished=false;
                 aft=timeinms()-start;
+                insert(p[i],filename);
             }
             else{
                 aft=timeinms()-start;
@@ -317,19 +367,29 @@ void MultiLevelFeedbackQueue(int quantum0,int quantum1,int quantum2,int boostTim
                 p[i].burst_time+=aft-conttime;
                 p[i].waiting_time=p[i].turnaround_time-p[i].burst_time;
                 insert(p[i],filename);
-                count++;
+                bool flag=false;
+                for(int j=0;j<fp;j++){
+                    if(strcmp(map[j].cmd,p[i].command)==0){
+                        map[j].avgbt=(map[j].tr*map[j].avgbt+ p[i].burst_time)/(map[j].tr+1);
+                        map[j].tr++;
+                        flag=true;
+                        break;
+                    }
+                }
+                if(!flag){
+                    map[fp].avgbt=p[i].burst_time;
+                    map[fp].cmd=strdup(p[i].command);
+                    map[fp].tr=1;
+                }
+                fp++;
             }
             RemoveNode(Q2);
             printf("In Priority 2 Queue | Command: %s | start_time of context: %ld | completion_time of context: %ld\n",p[i].command,conttime,aft);
-            present=timeinms()-start;
-            if(present-past>=boostTime){
-                UpgradeAllNode(Q1,Q2,Q3);
-                past=timeinms()-start;
-            }
+            
         }
         else if(Q3->FirstNode!=NULL){
 
-            int i=Q3->FirstNode->index;
+            int i=Q3->FirstNode->index;  
             if(p[i].started){
                 conttime=timeinms()-start;
                 kill(p[i].process_id,SIGCONT);
@@ -342,12 +402,12 @@ void MultiLevelFeedbackQueue(int quantum0,int quantum1,int quantum2,int boostTim
                 else if(r==0){
                     char *input=strdup(p[i].command);
                     char *myargs[512 + 1];
-                    char *token = strtok(input, " ");
+                    char *token3 = strtok(input, " ");
                     int j=0;
-                    while (token != NULL) {  
-                        myargs[j] = token;
+                    while (token3 != NULL) {  
+                        myargs[j] = token3;
                         j++;
-                        token = strtok(NULL, " ");
+                        token3 = strtok(NULL, " ");
                     }
                     myargs[j] = NULL;
                     execvp(myargs[0],myargs);
@@ -361,19 +421,30 @@ void MultiLevelFeedbackQueue(int quantum0,int quantum1,int quantum2,int boostTim
                     p[i].process_id=r;
                 } 
             }
+
             uint64_t aft;
-            usleep(quantum2*1000);
             int status;
-            int res=waitpid(p[i].process_id,&status,WNOHANG);
+            int res;
+            if(fp==count-1){
+                res=waitpid(p[i].process_id,&status,0);
+            }
+            else{
+                usleep(quantum2*1000);
+                res=waitpid(p[i].process_id,&status,WNOHANG);
+            }
+
             if(res==0){
                 kill(p[i].process_id,SIGSTOP);
                 aft=timeinms()-start;
                 p[i].burst_time+=aft-conttime;
+                Node* temp=newNode(i);
+                insertQueue(Q3,temp);
             } 
             else if(!WIFEXITED(status) || WEXITSTATUS(status)!=0){
                 p[i].error=true;
-                p[i].finished=true;
+                p[i].finished=false;
                 aft=timeinms()-start;
+                insert(p[i],filename);
             }
             else{
                 aft=timeinms()-start;
@@ -384,16 +455,66 @@ void MultiLevelFeedbackQueue(int quantum0,int quantum1,int quantum2,int boostTim
                 p[i].burst_time+=aft-conttime;
                 p[i].waiting_time=p[i].turnaround_time-p[i].burst_time;
                 insert(p[i],filename);
-                count++;
-                RemoveNode(Q3);
+                bool flag=false;
+                for(int j=0;j<fp;j++){
+                    if(strcmp(map[j].cmd,p[i].command)==0){
+                        map[j].avgbt=(map[j].tr*map[j].avgbt+ p[i].burst_time)/(map[j].tr+1);
+                        map[j].tr++;
+                        flag=true;
+                        break;
+                    }
+                }
+                if(!flag){
+                    map[fp].avgbt=p[i].burst_time;
+                    map[fp].cmd=strdup(p[i].command);
+                    map[fp].tr=1;
+                }
+                fp++;
             }
+            RemoveNode(Q3);
             printf("In Priority 3 Queue | Command: %s | start_time of context: %ld | completion_time of context: %ld\n",p[i].command,conttime,aft);
-            present=timeinms()-start;
-            if(present-past>=boostTime){
-                UpgradeAllNode(Q1,Q2,Q3);
-                past=timeinms()-start;
-            }
         }
-        input();
+        
+        present=timeinms()-start;
+        if(present-past>=boostTime){
+            UpgradeAllNode(Q1,Q2,Q3);
+            past=timeinms()-start;
+        }
+        memset(cmds, 0, sizeof(cmds));
+        taking_input(cmds);
+        char *token4 = strtok(cmds,"\n");
+        uint64_t itm=timeinms()-start;
+        while (token4 != NULL && strlen(token4)>0) {  
+            p[count].command=strdup(token4);
+            Node *temp=newNode(count);
+            bool flag=false;
+            for(int j=0;j<fp;j++){
+                if(strcmp(token4,map[j].cmd)==0){
+                    if(map[j].avgbt<=quantum0){
+                        insertQueue(Q1,temp);
+                    }
+                    else if(map[j].avgbt <=quantum1){
+                        insertQueue(Q2,temp);
+                    }
+                    else{
+                        insertQueue(Q3,temp);
+                    }
+                    flag=true;
+                    break;
+                }
+            }
+            if(!flag){
+                insertQueue(Q2,temp);
+            }
+            p[count].finished=false;
+            p[count].started=false;
+            p[count].error=false;
+            p[count].burst_time=0;
+            p[count].arrival_time=itm;
+            count++;
+            token4 = strtok(NULL, "\n");
+            
+        }
+
     }
 }
